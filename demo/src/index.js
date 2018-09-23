922c2127f4d3af8ce2825770751f36698fe6ff25
ngduc <ducjava@gmail.com>
2018-09-23 11:39:45 -0700

    feat: copy css files to "lib"; build: add postbuild;

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import '../../src/PxLogin.css';
+import '../../lib/styles.css';
 import PxLogin from '../../lib';
 
 class Demo extends Component {
